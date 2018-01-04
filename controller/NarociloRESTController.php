d7fadc5fa8ccb346ee6ae38f3112a724d6fc2682
MajZ15 <lenarsicmaj@gmail.com>
2018-01-04 13:49:15 +0100

    merge conflicts and pull problems

--- controller/NarociloRESTController.php
+++ controller/NarociloRESTController.php
@@ -3,7 +3,7 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: Content-Type");
 header("Content-Type: application/json");
 header("Access-Control-Allow-Methods: GET,POST,PUT,DELETE");
-##require_once("controller/BooksController.php");
+require_once("model/NarociloDB.php");
 require_once("ViewHelper.php");
 
 class NarociloRESTController {
